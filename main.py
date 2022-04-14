8d418825a1ca90d8455f819fa52607cc41be6e22
Katarzyna Górska <33665158+coyens@users.noreply.github.com>
2022-04-14 11:18:10 +0000

    gg

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("ddd")
+import requests 
+import datetime as DT
+
+today = DT.date.today()
+yesterday = today - DT.timedelta(days=1)
+link = 'https://newsapi.org/v2/everything?qInTitle=stock%20market&from=' + str(yesterday) +'&to=' + str(today) + '&sortBy=popularity&language=en&apiKey=890603a55bfa47048e4490069ebee18c'
+
+r = requests.get(link)
+
+
+content =r.json()
+print(link)
